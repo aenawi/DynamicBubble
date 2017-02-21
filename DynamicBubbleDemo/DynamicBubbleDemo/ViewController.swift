7bfd3bf8aff53ef4690e9e2eac5ae7dbba041352
Vlad Averin <hello@vladaverin.me>
2017-02-21 22:36:47 +0300

    Initial setup

---
+++ DynamicBubbleDemo/DynamicBubbleDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicBubbleDemo
+//
+//  Created by Vladislav Averin on 21/02/2017.
+//  Copyright © 2017 Vladislav Averin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
